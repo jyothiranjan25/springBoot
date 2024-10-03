d1ad1ced4547bec615ff5d2e88e7a3204f0ecafd
kumar jyothiranjan <jyothiranjan25@gmail.com>
2024-10-04 03:07:30 +0530

    Spring Boot Admin Implementation

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -1,10 +1,12 @@
 package com.example.springboot;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAdminServer
 @EnableJpaRepositories("com.example.springboot")
 public class Application {
 
